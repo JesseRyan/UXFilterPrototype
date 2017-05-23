c8c029fd26857f5bd2f96ca5165c0da97d4f9961
JesseRyan <hello@jesseryan-dev.com>
2017-05-22 23:09:26 -0700

    Adding filter tabs

--- app/src/main/java/com/jesseryan_dev/uxfilterprototype/MainActivity.java
+++ app/src/main/java/com/jesseryan_dev/uxfilterprototype/MainActivity.java
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         initViews();
-
 
         BottomSheetDialogFragment bottomSheetDialogFragment = new RefineFilterBottomSheetFragment();
         bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
